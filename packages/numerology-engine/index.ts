14814d7e7d3cfaf2d2d0aed0692fe3abcf033cdc
avi111 <avi111@gmail.com>
2020-12-28 00:19:46 +0200

    couple / business

--- packages/numerology-engine/index.ts
+++ packages/numerology-engine/index.ts
@@ -2,11 +2,15 @@ import Gematria from "./helpers/Gematria/Gematria";
 import MainTriangle from "./Calculations/mainTriangle";
 import Profile from "./MainTools/Profile";
 import Props, {Gender} from "./interfaces/props";
+import Couple from "./MainTools/Couple";
+import Business from "./MainTools/Business";
 
 const numerologyEngine = {
     Gematria,
     MainTriangle,
-    Profile
+    Profile,
+    Couple,
+    Business
 }
 
 export {Props as props};
