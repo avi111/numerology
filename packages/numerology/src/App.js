d5e07b31a532d15040cf35e5e85ccf54c5888251
Avi Levkovich <avi111@gmail.com>
2021-03-03 01:38:54 +0200

    material ui sketch

--- packages/numerology/src/App.js
+++ packages/numerology/src/App.js
@@ -1,8 +1,76 @@
 import './App.css'
+import React from "react";
 import {Profile} from "./numerologyEngine";
-import { Button } from '@material-ui/core';
+import {
+  Button,
+  Container,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles
+} from '@material-ui/core';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import clsx from 'clsx';
 
 function App() {
+  const useStyles = makeStyles({
+    list: {
+      width: 250,
+    },
+    fullList: {
+      width: 'auto',
+    },
+  });
+
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+      <div
+          className={clsx(classes.list, {
+            [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+          })}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+      >
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+          ))}
+        </List>
+      </div>
+  );
+
   const props = {
     birthDate: new Date('1980-9-16'),
     familyName: 'לבקוביץ',
@@ -19,12 +87,17 @@ function App() {
   const profile = new Profile(props);
 
   return (
-    <div className="App">
-      <header className="App-header">
+      <Container maxWidth="sm">
         <p>{JSON.stringify(profile.props.firstName)}</p>
-        <Button>goliath</Button>
-      </header>
-    </div>
+        {['left'].map((anchor) => (
+            <React.Fragment key={anchor}>
+              <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+              <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+                {list(anchor)}
+              </Drawer>
+            </React.Fragment>
+        ))}
+      </Container>
   );
 }
 
