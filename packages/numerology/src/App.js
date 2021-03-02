4e5c4d4c275812686ffe8bea935189b71af1deb5
Avi Levkovich <avi111@gmail.com>
2021-03-03 00:05:48 +0200

    add material ui

--- packages/numerology/src/App.js
+++ packages/numerology/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg'
 import './App.css'
 import {Profile} from "./numerologyEngine";
+import { Button } from '@material-ui/core';
 
 function App() {
   const props = {
@@ -22,18 +23,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>{JSON.stringify(profile.props.firstName)}</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>goliath</Button>
       </header>
     </div>
   );
