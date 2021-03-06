6dde74768effefa5cf1ade82535f13b82d90d83f
Avi Levkovich <avi111@gmail.com>
2021-03-07 00:25:31 +0200

    composeform

--- packages/compose-form/index.tsx
+++ packages/compose-form/index.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {FormEvent} from "react";
 import {Schema} from "./interfaces/schema";
 import {InputType} from "./interfaces/inputType"
 
-const ComposeForm = (schema?: Schema): JSX.Element => {
-    return <div>{JSON.stringify(schema)}</div>;
+export interface ComposeFormProps {
+    schema?: Schema,
+    onSubmit?: (e: FormEvent) => void
+}
+
+const ComposeForm = ({schema, onSubmit}: ComposeFormProps): JSX.Element => {
+    return (
+        <form onSubmit={(event => {
+            event.preventDefault();
+            onSubmit && onSubmit(event);
+        })}>
+            <div>{JSON.stringify(schema)}</div>
+            <input type="submit" value="submit"/>
+        </form>
+    );
 }
 
 export default ComposeForm;
--- packages/numerology/src/components/Form/index.tsx
+++ packages/numerology/src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {FormEvent} from "react";
 import ComposeForm, {Schema} from "@maya259/compose-form";
 import {forms} from "./forms";
 import profile from "./profile";
@@ -12,7 +12,11 @@ export interface FormProps {
 }
 
 const Form = ({form}: FormProps) => {
-    return ComposeForm(Adapter[form] as Schema);
+    return ComposeForm({
+        schema: Adapter[form] as Schema, onSubmit: (e: FormEvent) => {
+            console.log(e);
+        }
+    });
 }
 
 export default Form;
