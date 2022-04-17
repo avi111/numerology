5d6133ea63302d8bf0c19f2b3999c1a44cfe0791
Avi Levkovich <avi111@gmail.com>
2022-04-18 00:08:35 +0300

    restart

--- packages/web/src/firebase.ts
+++ packages/web/src/firebase.ts
@@ -1,14 +1,14 @@
 import firebase from "firebase/compat";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDCu7Yqa-Nt7Mhm-1HSGGIQtxZcHKbVGfk",
-    authDomain: "numerology-ae61d.firebaseapp.com",
-    databaseURL: "https://numerology-ae61d.firebaseio.com",
-    projectId: "numerology-ae61d",
-    storageBucket: "numerology-ae61d.appspot.com",
-    messagingSenderId: "756589978042",
-    appId: "1:756589978042:web:3df0af7f497c0eb9357d83",
-    measurementId: "G-XCSNKJSXGP"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 class Firebase {
